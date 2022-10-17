bde83920f0848aac67639f81fde33c650141fd2a
Asdrubal2002 <henryasdrubalrodriguezmorales@gmail.com>
2022-10-17 18:27:29 -0500

    medidas

--- src/components/services/servicesElements.js
+++ src/components/services/servicesElements.js
@@ -9,10 +9,10 @@ export const ServicesContainer = styled.div`
     background: #010606;
 
     @media screen and (max-width: 786px){
-        height: 1100px;
+        height: 1800px;
     }
     @media screen and (max-width: 480px){
-        height: 1300px;
+        height: 1350px;
     }
   
 `;
